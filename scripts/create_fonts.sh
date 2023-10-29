7cbc49210294f26e4fd9a130276337c775850fcd
Brandon Liu <bdon@bdon.org>
2023-10-29 10:29:57 -0700

    add font creation script for Sans

---
+++ scripts/create_fonts.sh
@@ -0,0 +1,96 @@
+ROOT=notofonts.github.io-main/fonts/
+VARIANT=unhinted/ttf
+
+set -e
+
+font-maker --name "Sans Italic" ../fonts \
+$ROOT/NotoSans/$VARIANT/NotoSans-italic.ttf
+
+font-maker --name "Sans Medium" ../fonts \
+$ROOT/NotoSans/$VARIANT/NotoSans-Medium.ttf \
+$ROOT/NotoSansArmenian/$VARIANT/NotoSansArmenian-Medium.ttf \
+$ROOT/NotoSansHebrew/$VARIANT/NotoSansHebrew-Medium.ttf \
+$ROOT/NotoSansArabic/$VARIANT/NotoSansArabic-Medium.ttf \
+$ROOT/NotoSansThaana/$VARIANT/NotoSansThaana-Medium.ttf \
+$ROOT/NotoSansDevanagari/$VARIANT/NotoSansDevanagari-Medium.ttf \
+$ROOT/NotoSansBengali/$VARIANT/NotoSansBengali-Medium.ttf \
+$ROOT/NotoSansGurmukhi/$VARIANT/NotoSansGurmukhi-Medium.ttf \
+$ROOT/NotoSansGujarati/$VARIANT/NotoSansGujarati-Medium.ttf \
+$ROOT/NotoSansTamil/$VARIANT/NotoSansTamil-Medium.ttf \
+$ROOT/NotoSansTelugu/$VARIANT/NotoSansTelugu-Medium.ttf \
+$ROOT/NotoSansKannada/$VARIANT/NotoSansKannada-Medium.ttf \
+$ROOT/NotoSansMalayalam/$VARIANT/NotoSansMalayalam-Medium.ttf \
+$ROOT/NotoSansSinhala/$VARIANT/NotoSansSinhala-Medium.ttf \
+$ROOT/NotoSansThai/$VARIANT/NotoSansThai-Medium.ttf \
+$ROOT/NotoSansLao/$VARIANT/NotoSansLao-Medium.ttf \
+$ROOT/NotoSansMyanmar/$VARIANT/NotoSansMyanmar-Medium.ttf \
+$ROOT/NotoSansGeorgian/$VARIANT/NotoSansGeorgian-Medium.ttf \
+$ROOT/NotoSansEthiopic/$VARIANT/NotoSansEthiopic-Medium.ttf \
+$ROOT/NotoSansCherokee/$VARIANT/NotoSansCherokee-Medium.ttf \
+$ROOT/NotoSansKhmer/$VARIANT/NotoSansKhmer-Medium.ttf \
+$ROOT/NotoSansSymbols/$VARIANT/NotoSansSymbols-Medium.ttf \
+$ROOT/NotoSansCham/$VARIANT/NotoSansCham-Medium.ttf
+
+font-maker --name "Sans Regular" ../fonts \
+$ROOT/NotoSans/$VARIANT/NotoSans-Regular.ttf \
+$ROOT/NotoSansArmenian/$VARIANT/NotoSansArmenian-Regular.ttf \
+$ROOT/NotoSansHebrew/$VARIANT/NotoSansHebrew-Regular.ttf \
+$ROOT/NotoSansArabic/$VARIANT/NotoSansArabic-Regular.ttf \
+$ROOT/NotoSansSyriac/$VARIANT/NotoSansSyriac-Regular.ttf \
+$ROOT/NotoSansThaana/$VARIANT/NotoSansThaana-Regular.ttf \
+$ROOT/NotoSansNKo/$VARIANT/NotoSansNKo-Regular.ttf \
+$ROOT/NotoSansSamaritan/$VARIANT/NotoSansSamaritan-Regular.ttf \
+$ROOT/NotoSansDevanagari/$VARIANT/NotoSansDevanagari-Regular.ttf \
+$ROOT/NotoSansBengali/$VARIANT/NotoSansBengali-Regular.ttf \
+$ROOT/NotoSansGurmukhi/$VARIANT/NotoSansGurmukhi-Regular.ttf \
+$ROOT/NotoSansGujarati/$VARIANT/NotoSansGujarati-Regular.ttf \
+$ROOT/NotoSansOriya/$VARIANT/NotoSansOriya-Regular.ttf \
+$ROOT/NotoSansTamil/$VARIANT/NotoSansTamil-Regular.ttf \
+$ROOT/NotoSansTelugu/$VARIANT/NotoSansTelugu-Regular.ttf \
+$ROOT/NotoSansKannada/$VARIANT/NotoSansKannada-Regular.ttf \
+$ROOT/NotoSansMalayalam/$VARIANT/NotoSansMalayalam-Regular.ttf \
+$ROOT/NotoSansSinhala/$VARIANT/NotoSansSinhala-Regular.ttf \
+$ROOT/NotoSansThai/$VARIANT/NotoSansThai-Regular.ttf \
+$ROOT/NotoSansLao/$VARIANT/NotoSansLao-Regular.ttf \
+$ROOT/NotoSerifTibetan/$VARIANT/NotoSerifTibetan-Regular.ttf \
+$ROOT/NotoSansMyanmar/$VARIANT/NotoSansMyanmar-Regular.ttf \
+$ROOT/NotoSansGeorgian/$VARIANT/NotoSansGeorgian-Regular.ttf \
+$ROOT/NotoSansEthiopic/$VARIANT/NotoSansEthiopic-Regular.ttf \
+$ROOT/NotoSansCherokee/$VARIANT/NotoSansCherokee-Regular.ttf \
+$ROOT/NotoSansCanadianAboriginal/$VARIANT/NotoSansCanadianAboriginal-Regular.ttf \
+$ROOT/NotoSansOgham/$VARIANT/NotoSansOgham-Regular.ttf \
+$ROOT/NotoSansRunic/$VARIANT/NotoSansRunic-Regular.ttf \
+$ROOT/NotoSansTagalog/$VARIANT/NotoSansTagalog-Regular.ttf \
+$ROOT/NotoSansHanunoo/$VARIANT/NotoSansHanunoo-Regular.ttf \
+$ROOT/NotoSansBuhid/$VARIANT/NotoSansBuhid-Regular.ttf \
+$ROOT/NotoSansTagbanwa/$VARIANT/NotoSansTagbanwa-Regular.ttf \
+$ROOT/NotoSansKhmer/$VARIANT/NotoSansKhmer-Regular.ttf \
+$ROOT/NotoSansMongolian/$VARIANT/NotoSansMongolian-Regular.ttf \
+$ROOT/NotoSansLimbu/$VARIANT/NotoSansLimbu-Regular.ttf \
+$ROOT/NotoSansTaiLe/$VARIANT/NotoSansTaiLe-Regular.ttf \
+$ROOT/NotoSansNewTaiLue/$VARIANT/NotoSansNewTaiLue-Regular.ttf \
+$ROOT/NotoSansBuginese/$VARIANT/NotoSansBuginese-Regular.ttf \
+$ROOT/NotoSansTaiTham/$VARIANT/NotoSansTaiTham-Regular.ttf \
+$ROOT/NotoSansBalinese/$VARIANT/NotoSansBalinese-Regular.ttf \
+$ROOT/NotoSansSundanese/$VARIANT/NotoSansSundanese-Regular.ttf \
+$ROOT/NotoSansBatak/$VARIANT/NotoSansBatak-Regular.ttf \
+$ROOT/NotoSansLepcha/$VARIANT/NotoSansLepcha-Regular.ttf \
+$ROOT/NotoSansOlChiki/$VARIANT/NotoSansOlChiki-Regular.ttf \
+$ROOT/NotoSansSymbols/$VARIANT/NotoSansSymbols-Regular.ttf \
+$ROOT/NotoSansSymbols2/$VARIANT/NotoSansSymbols2-Regular.ttf \
+$ROOT/NotoSansGlagolitic/$VARIANT/NotoSansGlagolitic-Regular.ttf \
+$ROOT/NotoSansCoptic/$VARIANT/NotoSansCoptic-Regular.ttf \
+$ROOT/NotoSansTifinagh/$VARIANT/NotoSansTifinagh-Regular.ttf \
+$ROOT/NotoSansYi/$VARIANT/NotoSansYi-Regular.ttf \
+$ROOT/NotoSansLisu/$VARIANT/NotoSansLisu-Regular.ttf \
+$ROOT/NotoSansVai/$VARIANT/NotoSansVai-Regular.ttf \
+$ROOT/NotoSansBamum/$VARIANT/NotoSansBamum-Regular.ttf \
+$ROOT/NotoSansSylotiNagri/$VARIANT/NotoSansSylotiNagri-Regular.ttf \
+$ROOT/NotoSansPhagsPa/$VARIANT/NotoSansPhags-Pa-Regular.ttf \
+$ROOT/NotoSansSaurashtra/$VARIANT/NotoSansSaurashtra-Regular.ttf \
+$ROOT/NotoSansKayahLi/$VARIANT/NotoSansKayahLi-Regular.ttf \
+$ROOT/NotoSansRejang/$VARIANT/NotoSansRejang-Regular.ttf \
+$ROOT/NotoSansJavanese/$VARIANT/NotoSansJavanese-Regular.ttf \
+$ROOT/NotoSansCham/$VARIANT/NotoSansCham-Regular.ttf \
+$ROOT/NotoSansTaiViet/$VARIANT/NotoSansTaiViet-Regular.ttf \
+$ROOT/NotoSansMeeteiMayek/$VARIANT/NotoSansMeeteiMayek-Regular.ttf
